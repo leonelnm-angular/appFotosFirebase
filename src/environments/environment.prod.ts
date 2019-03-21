42fe91393076a88720dcfb2de87cdb643f163dd7
Erik Navarrete <leonavas1995@gmail.com>
2019-03-21 21:53:18 +0100

    firebase environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB4mWZ3GCr90REjQbpLJRfYUEsUiGBsOQM',
+    authDomain: 'fotosapp-dbea6.firebaseapp.com',
+    databaseURL: 'https://fotosapp-dbea6.firebaseio.com',
+    projectId: 'fotosapp-dbea6',
+    storageBucket: 'fotosapp-dbea6.appspot.com',
+    messagingSenderId: '404295104866'
+
+  }
 };
